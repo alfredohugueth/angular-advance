f77d20c09044a9c28bee428b0ebe19b07f014364
Alfredo Hugueth <alfredohugueth99@gmail.com>
2021-04-25 09:15:02 -0500

    section 4-36

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 
 
 /* Components*/
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 /* Rutas */
 
 const routes:Routes = [
   
-  {path: 'register', component:RegisterComponent},
-  {path: 'login', component:LoginComponent},
-
+  /* Path: 'dashboard' PagesRouting */
+  /* Path: 'auth' AuthRouting */
+  
   {path: '**', component:NopagefoundComponent},
 
 ]
@@ -24,7 +24,8 @@ const routes:Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot( routes ),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[ RouterModule ]
 })
---
+++ src/app/auth/auth.routing.ts
@@ -0,0 +1,20 @@
+/* Modules */
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+/* Components*/
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const routes: Routes = [
+    {path: 'register', component:RegisterComponent},
+    {path: 'login', component:LoginComponent},
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule {}
