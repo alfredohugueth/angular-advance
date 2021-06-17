74c32edaa0a821f56c490900949470aad9a5d7a8
Alfredo Hugueth <alfredohugueth99@gmail.com>
2021-06-16 22:02:43 -0500

    Section 14-164

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,12 +8,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   public registerForm = this.fb.group({
     
-    nombre : [ 'Fernando', [ Validators.required, Validators.minLength(3) ] ],
-    email : [ 'test100@gmail.com', [ Validators.required, Validators.minLength(3) ] ],
-    password : [ '1234567', [ Validators.required, Validators.minLength(3) ]],
-    password2 : [ '1234567', [ Validators.required, Validators.minLength(3) ]],
+    nombre : [ 'Alfredo', [ Validators.required, Validators.minLength(3) ] ],
+    email : [ 'test100@gmail.com', [ Validators.required, Validators.minLength(3), Validators.email ] ],
+    password : [ '', [ Validators.required, Validators.minLength(3) ]],
+    password2 : [ '', [ Validators.required, Validators.minLength(3) ]],
     terminos: [ false, [ Validators.required, Validators.minLength(3) ]]
 
   });
@@ -22,7 +24,40 @@ export class RegisterComponent {
   constructor( private fb: FormBuilder) { }
 
   crearUsuario() {
+    
+    this.formSubmitted = true;
     console.log( this.registerForm.value );
+
+    if ( this.registerForm.valid ) {
+      
+      console.log(' ----> Posteando formulario ');
+    
+    } else {
+
+      console.log(' ----> Formulario no valido ');
+
+    }
+  
+  }
+
+  campoNoValido( campo: string ) : boolean {
+
+    if ( this.registerForm.get( campo )?.invalid && this.formSubmitted ) {
+      
+      return true;
+    
+    } else {
+
+      return false;
+      
+    }
+
+  }
+
+  aceptaTerminos () {
+
+    return !this.registerForm.get( 'terminos' )?.value && this.formSubmitted;
+
   }
 
   
