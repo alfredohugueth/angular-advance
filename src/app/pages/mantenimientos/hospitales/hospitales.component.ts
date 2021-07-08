8c17961d807d0d662f24e760d7bf771beeb1d06f
Alfredo Hugueth <alfredohugueth99@gmail.com>
2021-07-08 07:45:37 -0500

    Section 17-220

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,6 +1,10 @@
+import Swal from 'sweetalert2';
+import { delay } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Hospital } from 'src/app/models/hospital.model';
 import { HospitalService } from 'src/app/services/hospital.service';
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
 
 @Component({
   selector: 'app-hospitales',
@@ -12,13 +16,25 @@ export class HospitalesComponent implements OnInit {
 
   public hospitales : Hospital[] = [];
   public cargando : boolean = true;
+  public imgSubs! : Subscription;
 
-  constructor( private hospitalService : HospitalService ) { }
+  constructor( private hospitalService : HospitalService, private modalImageService : ModalImagenService ) { }
 
   ngOnInit(): void 
   {
 
     this.cargarHospitales();
+
+    this.imgSubs = this.modalImageService.nuevaImagen
+    .pipe(
+      delay(200)
+    )
+    .subscribe( img => 
+      {
+        
+        this.cargarHospitales();
+
+      })
 
   }
 
@@ -35,4 +51,59 @@ export class HospitalesComponent implements OnInit {
 
   }
 
+  guardarCambios( hospital : Hospital )
+  {
+    console.log( hospital );
+    this.hospitalService.actualizarHospital( hospital._id, hospital.nombre )
+    .subscribe( res => 
+      {
+
+        this.cargarHospitales();
+        Swal.fire( 'Actualizado', `Se actualizo el hospital con nombre ${ hospital.nombre }`, 'success' );
+
+      })
+  }
+
+  eliminarHospital( hospital : Hospital )
+  {
+    console.log( hospital );
+    this.hospitalService.borrarHospital( hospital._id )
+    .subscribe( res => 
+      {
+        
+        Swal.fire( 'Borrado', `Se Borro el hospital con nombre ${ hospital.nombre }`, 'success' );
+        this.cargarHospitales();
+
+      })
+  }
+
+  async abrirSweetAlert()
+  {
+    const { value } = await Swal.fire<string>({
+      title : 'Crear Hospital',
+      text : 'Ingrese el nombre del hospital',
+      input: 'text',
+      inputPlaceholder: 'Nombre del hospital',
+      showCancelButton : true
+    })
+    
+    if ( value?.trim().length )
+    {
+      this.hospitalService.crearHospital( value )
+      .subscribe( res => 
+        {
+          
+          this.hospitales.push( res.hospital );
+
+        })
+    }
+
+  }
+
+  abrirModal( hospital : Hospital)
+  {
+
+    this.modalImageService.abrirModal( 'hospitales', hospital._id, hospital.img );
+
+  }
 }
